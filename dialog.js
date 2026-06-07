0485973bb43c22b9d26f17d54332ef241eca84a5
DoomFishy <calvinlu350@gmail.com>
2026-06-06 20:52:29 -0400

    Finished implementing project functions

--- dialog.js
+++ dialog.js
@@ -1,21 +1,31 @@
-const app = document.getElementById("preview");
-const preview = document.querySelectorAll(".previewable");
+const preview = document.getElementById("preview");
+const preview_obj = document.querySelectorAll(".previewable");
 
-preview.forEach(item =>{
-    item.addEventListener("click", change_preview)
+const preview_website = document.getElementById("preview-website");
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" && !preview.classList.contains("hidden")) {
+        preview.classList.add("hidden");
+    }
 });
 
-app.addEventListener("click", function(){
-    app.innerHTML = "";
-    app.classList.add("hidden");
+preview.addEventListener("click", function(e){
+    if (e.target === preview) {
+        preview.classList.add("hidden");
+    }
+});
+
+preview_obj.forEach(item =>{
+    item.addEventListener("click", change_preview)
 });
 
 async function loadHtmlFile(filePath){
     try{
         const response = await fetch(filePath);
         const html = await response.text();
-        app.innerHTML = html;
-        app.classList.remove("hidden");
+        preview.innerHTML = html;
+        preview.classList.remove("hidden");
     } catch(error){
         console.error("Error loading HTML:", error);
     }
@@ -23,8 +33,6 @@ async function loadHtmlFile(filePath){
 
 function change_preview(event){
     const id = event.currentTarget.id;
-
-    console.log("clicked ", id);
 
     if (id == "preview-mmu"){
         loadHtmlFile("project_previews/mmu.html");
@@ -37,4 +45,29 @@ function change_preview(event){
     else if (id == "preview-labs"){
         loadHtmlFile("project_previews/lab_sec.html");
     }
+
+    else if (id == "preview-stego"){
+        loadHtmlFile("project_previews/stego.html");
+    }
+
+    else if (id == "preview-yu2025"){
+        loadHtmlFile("project_previews/yu_game.html");
+    }
+
+    else if (id == "preview-greaseTrap"){
+        loadHtmlFile("project_previews/grease_trap.html");
+    }
+
+    else if (id == "preview-abnb"){
+        loadHtmlFile("project_previews/abnb.html");
+    }
+}
+
+function open_website() {
+    const website_expanded = document.getElementById("preview-website-expanded");
+    const iframe = website_expanded.querySelector("iframe");
+
+    website_expanded.classList.remove("hidden");
+
+    iframe.src = "https://doomfishy.github.io/SimpleStegoJS/";
 }
